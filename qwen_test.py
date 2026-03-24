26ca133462b62a3a3b95457770f9cf91dd047e59
Vera S. Jackson <80920499+VeraJSolo@users.noreply.github.com>
2026-03-23 20:33:46 -0800

    pushing wip qwen_test script

---
+++ qwen_test.py
@@ -0,0 +1,44 @@
+from diffusers import QwenImageLayeredPipeline
+import torch
+from PIL import Image
+import argparse
+
+# Arguments
+parser = argparse.ArgumentParser(description="QWEN Segmentation")
+parser.add_argument("--image", type=str, required=True, help="Path to map image")
+parser.add_argument("--output", type=str, default="output_mask.png", help="Path to save mask")
+args = parser.parse_args()
+
+# Pipeline
+pipeline = QwenImageLayeredPipeline.from_pretrained("Qwen/Qwen-Image-Layered")
+pipeline = pipeline.to("cuda", torch.bfloat16)
+pipeline.set_progress_bar_config(disable=None)
+
+# Load image
+image = Image.open(args.image).convert("RGBA")
+
+# Inputs
+inputs = {
+    "image": image,
+    "generator": torch.Generator(device='cuda').manual_seed(777),
+    "true_cfg_scale": 4.0,
+    "negative_prompt": " ",
+    "num_inference_steps": 50,
+    "num_images_per_prompt": 1,
+    "layers": 3,
+    "resolution": 640,      # Using different bucket (640, 1024) to determine the resolution. For this version, 640 is recommended
+    "cfg_normalize": True,  # Whether enable cfg normalization.
+    "use_en_prompt": True,  # Automatic caption language if user does not provide caption
+}
+
+# Run model
+with torch.inference_mode():
+    output = pipeline(**inputs)
+    output_image = output.images
+
+# Save outputs
+import os
+os.makedirs(args.output_dir, exist_ok=True)
+
+for i, img in enumerate(output_image):
+    img.save(f"{args.output_dir}/{i}.png")
